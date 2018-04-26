1e791ccc2adfdadb894e98e6743cd2342a87a709
Andras Svraka <svraka.andras@gmail.com>
2018-04-26 16:12:37 +0200

    Git branch neve az üzenetben

--- bin/send_notification_after_build.sh
+++ bin/send_notification_after_build.sh
@@ -1,11 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+if [ -n "$1" ]
+then
+	MESSAGE=" ($1)"
+else
+	MESSAGE=""
+fi
 
 JOB=${PWD##*/}
 DATE=$(date --iso-8601=seconds)
+BRANCH=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/')
+
+DESCRIPTION="$JOB$BRANCH completed on $DATE$MESSAGE"
 
 curl \
 	-F apikey=b56b4b65b4bf3094f5e5c38debc679ffb12709ee \
 	-F application=Make \
 	-F event="Build complete" \
-	-F description="$JOB completed on $DATE" \
+	-F description="$DESCRIPTION" \
 	https://api.prowlapp.com/publicapi/add
